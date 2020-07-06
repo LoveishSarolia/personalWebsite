a0b62228e36daf2481feebf09de8806a8073464f
Loveish Sarolia <loveishsarolia@gmail.com>
2020-07-06 09:15:58 -0500

    fixed font issue

---
+++ website/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Home msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Armata');
+
+#app {
+  font-family: 'Armata';
+  font-size: 22px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #ffffff;
+}
+body {
+  background: #080808;
+}
+</style>
---
+++ website/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    Loveish Sarolia
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Armata');
+
+#app {
+  font-family: 'Armata';
+  font-size: 22px;
+  -webkit-font-smoothing: antialiased;
+  color: #ffffff;
+}
+body {
+  background: #080808;
+}
+</style>
